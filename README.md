d48b891020ae449d78a90932dcdedb0f922de7e4
Kristina Bekher <bekherkr@gmail.com>
2023-09-21 18:21:41 +0200

    clone todo app

--- README.md
+++ README.md
@@ -1,46 +1,22 @@
-# Getting Started with Create React App
+# React Todo App with API ✅
+React Todo App is built on React and uses RapidAPI. It has a intuitive and friendly user interface that helps you manage tasks efficiently.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Features
+- <strong>Intuitive Task Creation</strong>: Easily add and organize tasks with a clean and user-friendly interface.
+- <strong>Real-time Updates</strong>: Instantly see changes and updates without refreshing the page.
+- <strong>Task Completion</strong>: Mark tasks as complete and track your progress.
+- <strong>Task Editing</strong>: Modify task details on the fly, adapting to your evolving needs.
+- <strong>Filtering</strong>: Quickly find tasks with filtering options.
+- <strong>Data Persistence</strong>: Your tasks are saved and available when you return to the app.
 
-## Available Scripts
+# Technologies & Tools Used
+- React
+- Typescript
+- HTML
+- SCSS
+- JavaScript
+- VS Code
+- Rapid API
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+# Demo
+A live demo of the React Products Catalog App is available at [DEMO](https://kbekher.github.io/react-todo-app/) .
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,236 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, {
+  useEffect, useMemo, useRef, useState,
+} from 'react';
+import classNames from 'classnames';
 
-function App() {
+import { UserWarning } from './UserWarning';
+import * as todoService from './api/todos';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoFilter } from './components/TodoFilter/TodoFilter';
+import { useTodo } from './hooks/useTodo';
+import { ErrorMessage } from './types/ErrorMessage';
+import { Todo } from './types/Todo';
+
+const USER_ID = 11340;
+
+export const App: React.FC = () => {
+  const {
+    todos,
+    setTodos,
+    isChecked,
+    setIsChecked,
+    errorMessage,
+    setErrorMessage,
+    setIsProcessing,
+  } = useTodo();
+
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  });
+
+  useEffect(() => {
+    todoService.getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setErrorMessage(ErrorMessage.LOAD_ERROR));
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErrorMessage(ErrorMessage.DEFAULT);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [errorMessage]);
+
+  setIsChecked(todos.every(todo => todo.completed) && todos.length > 0);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage(ErrorMessage.DEFAULT);
+    setNewTodoTitle(event.target.value);
+  };
+
+  const addNewTodo = (newTitle: string) => {
+    const newTodoToAdd = {
+      id: 0,
+      userId: USER_ID,
+      title: (newTitle),
+      completed: false,
+    };
+
+    setTempTodo(newTodoToAdd);
+
+    todoService.addTodo(newTodoToAdd)
+      .then(newTodo => {
+        setTodos(currentTodos => [...currentTodos, newTodo]);
+      })
+      .catch((error) => {
+        setErrorMessage(ErrorMessage.ADD_ERROR);
+        throw error;
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setNewTodoTitle('');
+      });
+  };
+
+  const deleteSelectedTodo = (todoId: number): void => {
+    setIsProcessing(currentIds => [...currentIds, todoId]);
+
+    todoService.deleteTodo(todoId)
+      .then(() => {
+        setTodos(curentTodos => curentTodos.filter(item => item.id !== todoId));
+      })
+      .catch((error) => {
+        setTodos(todos);
+        setErrorMessage(ErrorMessage.DELETE_ERROR);
+        throw error;
+      })
+      .finally(() => setIsProcessing([]));
+  };
+
+  const updateSelectedTodo = (updatedTodo: Todo): void => {
+    setIsProcessing(currentIds => [...currentIds, updatedTodo.id]);
+
+    todoService.updateTodo(updatedTodo)
+      .then(todo => {
+        setTodos(currentTodos => {
+          const newTodos = [...currentTodos];
+          const index = newTodos.findIndex(item => item.id === updatedTodo.id);
+
+          newTodos.splice(index, 1, todo);
+
+          return newTodos;
+        });
+      })
+      .catch((error) => {
+        setErrorMessage(ErrorMessage.UPDATE_ERROR);
+        throw error;
+      })
+      .finally(() => {
+        setIsProcessing([]);
+        setIsChecked(!isChecked);
+      });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newTitle = newTodoTitle.trim();
+
+    if (!newTitle) {
+      setErrorMessage(ErrorMessage.TITLE_ERROR);
+      setNewTodoTitle('');
+
+      return;
+    }
+
+    addNewTodo(newTitle);
+  };
+
+  const handleCheckAllTodos = () => {
+    todos.forEach(todo => {
+      const updatedTodo = {
+        ...todo,
+        completed: !isChecked,
+      };
+
+      updateSelectedTodo(updatedTodo);
+    });
+  };
+
+  const activeTodosCounter = useMemo(() => {
+    return todos.filter(todo => !todo.completed).length;
+  }, [todos]);
+
+  const completedTodosCounter = useMemo(() => {
+    return todos.filter(todo => todo.completed).length;
+  }, [todos]);
+
+  const clearCompletedTodos = () => {
+    todos.forEach(todo => todo.completed && deleteSelectedTodo(todo.id));
+  };
+
+  const handleDeleteErrorMessage = () => {
+    setErrorMessage(ErrorMessage.DEFAULT);
+  };
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          <button
+            type="button"
+            className={classNames('todoapp__toggle-all', {
+              active: isChecked,
+            })}
+            onClick={handleCheckAllTodos}
+          />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={inputRef}
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+              value={newTodoTitle}
+              onChange={handleTitleChange}
+              disabled={tempTodo !== null}
+            />
+          </form>
+        </header>
+
+        {(todos.length > 0 || tempTodo !== null) && (
+          <>
+            <TodoList tempTodo={tempTodo} />
+
+            <footer className="todoapp__footer">
+              <span className="todo-count">
+                {`${activeTodosCounter} item${activeTodosCounter === 1 ? '' : 's'} left`}
+              </span>
+
+              <TodoFilter />
+
+              <button
+                type="button"
+                className="todoapp__clear-completed"
+                onClick={clearCompletedTodos}
+                disabled={completedTodosCounter < 0}
+              >
+                {completedTodosCounter > 0 && 'Clear completed'}
+              </button>
+            </footer>
+          </>
+        )}
+      </div>
+
+      {errorMessage && (
+        <div className={classNames(
+          'notification is-danger is-light has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
         >
-          Learn React
-        </a>
-      </header>
+          <button
+            type="button"
+            className="delete"
+            onClick={handleDeleteErrorMessage}
+          />
+          {errorMessage}
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
+};
