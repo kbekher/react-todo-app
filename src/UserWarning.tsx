5c65a82f9c791e58fd3f4309bc5ea4a960756c89
Kristina Bekher <bekherkr@gmail.com>
2023-09-23 12:18:36 +0200

    fix spaces

--- src/UserWarning.tsx
+++ src/UserWarning.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 export const UserWarning: React.FC = () => (
-  <section className="section">
+   <section className="section">
     <p className="box is-size-3">
       Please get your
-      {' '}
-      <b> userId </b>
-      {' '}
-      <a href="https://mate-academy.github.io/react_student-registration">
+      <b className="mx-1"> userId </b>
+      <a 
+        href="https://mate-academy.github.io/react_student-registration"
+        className="mx-1"
+      >
         here
       </a>
-      {' '}
       and save it in the app
-      {' '}
-      <pre>const USER_ID = ...</pre>
+      <pre className="mx-1">const USER_ID = ...</pre>
 
       All requests to the API must be sent with this
       <b> userId.</b>
